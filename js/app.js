17b26ab7693853bd0cc231a730888623ea376cd9
luGar <garrluis@gmail.com>
2021-06-25 22:09:00 -0400

    Validación de ruta de inicio;

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+var url = window.location.href;
+var swLocation = '/Twittor/sw.js';
+
+
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
+
 }
 
 
